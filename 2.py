207caaf6a587fa04dd34f9ba107a198a6ce91100
Omkar Dorugade <100912340+omkar5252@users.noreply.github.com>
2022-03-15 10:24:29 +0530

    Area of rectangle

--- 2.py
+++
@@ -1,7 +0,0 @@
-# Problem 2 : calculate area of rectangle based on length and breadth.
-
-l = float(input("Enter lenght:"))
-b = float(input("Enter breadth:"))
-
-a = l * b
-print("Area of Rectangle is",a)
